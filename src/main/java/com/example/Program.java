1ca1a1317974c242cea19efa22409ea6e943ea16
Gleb <glebkabaev200307@gmail.com>
2025-05-21 19:24:01 +0300

    testing new IDE

--- src/main/java/com/example/Program.java
+++ src/main/java/com/example/Program.java
@@ -6,5 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Program{
-    public static void main(String[] args) {SpringApplication.run(Program.class, args);}
+    public static void main(String[] args) {SpringApplication.run(Program.class, args);
+    }
 }
